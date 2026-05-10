6dc21b2343b40fa4b9c401174068702f11af59ee
Willy-557 <williamfsetiadi@gmail.com>
2026-05-10 13:09:46 +0700

    edit the scriptCart.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let cart = [];
-let isiKeranjang = document.querySelector('#item1');
+
 
 const produk = [ 
     { id: 1, kategori: "Kaos", jenisKelamin: "Pria", sHarga: "Rp 129.000", ukuran: "XS-3XL", nama: "T-Shirt Soft Touch Kerah Bulat Lengan Panjang", harga: 129000, image: "Asset_Tugas/Pria/Produk1.jpg", cekDiskon: true, hargaSebelumDiskon: "Rp 199.000", margin: 0, padding: 23}, 
@@ -71,7 +70,7 @@ function renderTampilanDepan(kategori, jenisKelamin, harga) {
                         <h5 class="fw-bold fs-6">${p.nama}</h5>
                         <h6 class="text-danger fw-bold fs-5">${p.sHarga}</h6>
                         <h6 class="fw-bold text-secondary"><strike>${p.hargaSebelumDiskon}</strike></h6>
-                        <button type="button" class="btn bg-black text-white" style="position: absolute; padding-right: ${p.padding}px; margin-top: ${p.margin}px;" onclick="tambahKeCart('${p.image}', '${p.nama}', '${p.jenisKelamin}, ${p.ukuran}', '${p.sHarga}')">Tambah Ke Keranjang</button>
+                        <button type="button" class="btn bg-black text-white" style="position: absolute; padding-right: ${p.padding}px; margin-top: ${p.margin}px;" onclick="tambahKeCart('${p.image}', '${p.nama}', '${p.jenisKelamin}, ${p.ukuran}', '${p.sHarga}', '${p.harga}', '1')">Tambah Ke Keranjang</button>
                     </div>
                 </div>
             `;
@@ -85,7 +84,7 @@ function renderTampilanDepan(kategori, jenisKelamin, harga) {
                         <p class="text-secondary">${p.jenisKelamin}, ${p.ukuran}</p>
                         <h5 class="fw-bold fs-6">${p.nama}</h5>
                         <h6 class="text-dark fw-bold fs-5 ">${p.sHarga}</h6>
-                        <button type="button" class="btn bg-black text-white" style="position: absolute; padding-right: ${p.padding}px; margin-top: ${p.margin}px;" onclick="tambahKeCart('${p.image}', '${p.nama}', '${p.jenisKelamin}, ${p.ukuran}', '${p.sHarga}')">Tambah Ke Keranjang</button>
+                        <button type="button" class="btn bg-black text-white" style="position: absolute; padding-right: ${p.padding}px; margin-top: ${p.margin}px;" onclick="tambahKeCart('${p.image}', '${p.nama}', '${p.jenisKelamin}, ${p.ukuran}', '${p.sHarga}', '${p.harga}', '1')">Tambah Ke Keranjang</button>
                     </div>
                 </div>
             `;
--- scriptCart.js
+++ scriptCart.js
@@ -1,3 +1,9 @@
+let cart = [];
+
+let isiKeranjang = document.querySelector('#item1');
+isiKeranjang.innerHTML = ""
+let total = 0;
+
 function renderCart() {
     if (cart.length == 0) {
         isiKeranjang.innerHTML = `
@@ -9,11 +15,51 @@ function renderCart() {
                 </div>
             </div>
         `;
+    } else {
+        cart.forEach(c => {
+            isiKeranjang.innerHTML += `
+                <div class="d-flex" style="margin: 10px 0px;">
+                    <img src="${c.image}" class="img-fluid rounded-start" alt="...">
+                    <div class="w-100 shadow-sm">
+                        <div class="d-flex justify-content-between" style="margin-top: 150px;">
+                            <div class="item ms-3">
+                                <h5 class="card-title">${c.namaBaju}</h5>
+                                <p class="card-text">${c.gender}, ${c.ukuranBaju}</p>
+                                <p class="card-text text-body-secondary">${c.StringHarga}</p>
+                            </div>
+                            <div class="item d-flex">
+                                <button class="btn btn-outline-secondary" style="margin-top: 30px; margin-bottom: 70px;" type="button">-</button>
+                                <p style="margin: 37px 10px;">${c.jumlahPesan}</p>
+                                <button class="btn btn-outline-secondary" style="margin-top: 30px; margin-bottom: 70px;" type="button">+</button>
+                            </div>
+                            <div class="item mt-1 me-3">
+                                <h6 class="fw-bold">${c.jumlahPesan * c.hargaNum}</h6>
+                                <button type="button" class="btn btn-danger mt-3 ms-3">Hapus</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }) 
     }
 }
 
-function tambahKeCart(gambar, namaBaju, ukuran, harga) {
-    
+function tambahKeCart(gambar, nama, jenisKelamin, ukuran, sHarga, harga, jumlah) {
+    cart.forEach(c => {
+        if (c.namaBaju != nama) {
+            total += harga;
+
+            cart.push({
+                image: gambar,
+                namaBaju: nama, 
+                gender: jenisKelamin,
+                ukuranBaju: ukuran,
+                StringHarga: sHarga,
+                hargaNum : harga,
+                jumlahPesan: jumlah
+            })  
+        }
+    })
 }
 
 let btnDeleteAll = document.querySelector('#btnDeleteAll');
@@ -22,3 +68,8 @@ btnDeleteAll.addEventListener("click", function(e) {
     cart = [];
     renderCart();
 })
+
+let ambilTotalHarga = document.querySelector("#totalHarga");
+ambilTotalHarga.innerHTML = total;
+
+renderCart();
